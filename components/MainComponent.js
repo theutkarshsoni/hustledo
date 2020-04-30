2f0e3948ea3f4c707d05c9d8f6f1234de5c588c3
Utkarsh Soni <utkarshsoni12@gmail.com>
2020-04-30 21:53:52 +0530

    Add Naivgation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,10 +1,80 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import More from './MoreComponent';
+import { Icon } from 'react-native-elements';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const HomeNavigator = createStackNavigator();
+const MoreNavigaotr = createStackNavigator();
+const MainNavigator = createDrawerNavigator();
+
+function getHomeNavigator({ navigation }) {
+    return(
+        <HomeNavigator.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#57A2CC',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    color: "#fff"
+                },
+                headerLeft: () => <Icon name='bars' type='font-awesome' color='white' containerStyle={{ paddingLeft: 15 }} onPress={() => navigation.toggleDrawer()} />
+            }}
+        >
+            <HomeNavigator.Screen name="Welcome, Hustler!" component={Home} />
+        </HomeNavigator.Navigator>
+    );
+}
+
+function getMoreNavigator({ navigation }) {
+    return(
+        <MoreNavigaotr.Navigator
+            initialRouteName="More"
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#57A2CC',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                    color: "#fff"
+                },
+                headerLeft: () => <Icon name='bars' type='font-awesome' color='white' containerStyle={{ paddingLeft: 15 }} onPress={() => navigation.toggleDrawer()} />
+            }}
+        >
+            <MoreNavigaotr.Screen name="More" component={More} />
+        </MoreNavigaotr.Navigator>
+    );
+}
 
 class Main extends Component {
     render(){
         return(
-            <Home />
+            <NavigationContainer>
+                <MainNavigator.Navigator 
+                    drawerStyle={{ 
+                        backgroundColor: '#57A2CC'
+                    }} 
+                    drawerContentOptions={{
+                        labelStyle:{
+                            color: '#fff',
+                            fontSize: 16
+                        },
+                        style:{
+                            paddingTop: 20
+                        }
+                    }}>
+                    <MainNavigator.Screen name="Home" component={getHomeNavigator} 
+                        options={{ drawerLabel: 'Home', drawerIcon: () => <Icon name='home' type='font-awesome' color='white' />}}></MainNavigator.Screen>
+                    <MainNavigator.Screen name="More" component={getMoreNavigator} 
+                        options={{ drawerLabel: 'More', drawerIcon: () => <Icon name='plus' type='font-awesome' color='white' />}}></MainNavigator.Screen>
+                </MainNavigator.Navigator>
+            </NavigationContainer>
         );
     }
 }
---
+++ components/MoreComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class More extends Component {
+    render(){
+        return(
+            <View style={{flex: 1}}>
+                <Text>More</Text>
+            </View>
+        );
+    }
+}
+
+export default More;
