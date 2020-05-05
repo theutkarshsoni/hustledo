127e685d00363a734c8ad73f7daf70e9df249400
Utkarsh Soni <utkarshsoni12@gmail.com>
2020-05-05 14:31:29 +0530

    Update Yoga View

--- components/YogaComponent.js
+++ components/YogaComponent.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Tile } from 'react-native-elements';
 
 class Yoga extends Component {
     render() {
         return(
-            <View>
-                <Text>Yoga</Text>
+            <View style={{ flex: 1}}>
+                <View style={{ flex: 1, borderBottomWidth: 3, borderColor: "white" }}>
+                    <Tile
+                        imageSrc={require('../assets/home_yoga_1.webp')}
+                        title='Poses'
+                        caption='Freedom to practice whatever you want'
+                        featured
+                        titleStyle={{
+                            fontSize: 30,
+                            fontWeight: 'bold'
+                        }}
+                        imageContainerStyle={{
+                            backgroundColor: 'rgba(87, 162, 204, 0.5)'
+                        }}
+                    />
+                </View>
+                <View style={{ flex: 1, borderTopWidth: 3, borderColor: "white" }}>
+                    <Tile
+                        imageSrc={require('../assets/home_yoga_2.webp')}
+                        title='Classes'
+                        caption='Focus on flexibility, strength and balance'
+                        featured
+                        titleStyle={{
+                            fontSize: 30,
+                            fontWeight: 'bold'
+                        }}
+                        imageContainerStyle={{
+                            backgroundColor: 'rgba(87, 162, 204, 0.5)'
+                        }}
+                    />
+                </View>
             </View>
         );
     }
